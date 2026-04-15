e38522fb48913c9e871f750307af3f41391a17aa
Trimunajaty <trimunajaty@gmail.com>
2026-04-15 11:00:19 +0700

    mengkonversi line endings dari LF (Linux) ke CRLF (Windows) karena kamu pakai Windows

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<php?>
+<?php
+require_once 'config.php';
+
+if (isset($_SESSION['role'])) {
+  if ($_SESSION['role'] === 'admin') {
+    header('Location: ' . base_url('/admin/dashboard.php'));
+  } else {
+    header('Location: ' . base_url('/mahasiswa/dashboard.php'));
+  }
+} else {
+  header('Location: ' . base_url('/login.php'));
+}
+exit;
+?>
